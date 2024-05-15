d2651bf848d0487f90246aa925355dd9c2b85757
lancealdaba <130742471+lancealdaba@users.noreply.github.com>
2024-05-15 21:30:44 +0800

    update main.py and main.kv
    inupdate yung screen size, size ng text input, at colors

--- HappyKid/kivy_venv/HAPPY KID DEISGN LOGIN/main.py
+++ HappyKid/kivy_venv/HAPPY KID DEISGN LOGIN/main.py
@@ -1,43 +1,31 @@
 from kivy.app import App
 from kivy.lang import Builder
 from kivy.uix.screenmanager import ScreenManager, Screen
+from kivy.core.window import Window
+
+Window.size = (420,630)
 
 Builder.load_file("main.kv")
 
 class LoginScreen(Screen):
     def create_account(self):
         self.manager.current = 'create_account'
-        
-class CreateAccountScreen(Screen):
-    pass
 
 class CreateAccountScreen(Screen):
-    def consultation_form(self):
-        self.manager.current = 'consultation_form'
-        
-class ConsultationForm(Screen):
     pass
 
-class ConsultationForm(Screen):
-    def consultation_form2(self):
-        self.manager.current = 'consultation_form2'
-        
-class ConsultationForm2(Screen):
+class BookAppointment(Screen):
     pass
-
-
 
 class TestApp(App):
     def build(self):
         self.sm = ScreenManager()
         self.login_screen = LoginScreen(name='login')
         self.create_account_screen = CreateAccountScreen(name='create_account')
-        self.consultation_form = ConsultationForm(name='consultation_form')
-        self.consultation_form2 = ConsultationForm2(name='consultation_form2')
+        self.book_appointment_screen = BookAppointment(name='book')  # Corrected typo here
         self.sm.add_widget(self.login_screen)
         self.sm.add_widget(self.create_account_screen)
-        self.sm.add_widget(self.consultation_form)
-        self.sm.add_widget(self.consultation_form2)
+        self.sm.add_widget(self.book_appointment_screen)
         return self.sm
 
 if __name__ == '__main__':
