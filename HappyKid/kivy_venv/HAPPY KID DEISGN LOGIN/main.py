4bb91189cc0a52f9f056d5364e27082709a9ac36
lancealdaba <130742471+lancealdaba@users.noreply.github.com>
2024-05-17 21:00:43 +0800

    Update
    apdit

--- HappyKid/kivy_venv/HAPPY KID DEISGN LOGIN/main.py
+++ HappyKid/kivy_venv/HAPPY KID DEISGN LOGIN/main.py
@@ -14,6 +14,10 @@ class LoginScreen(Screen):
 class CreateAccountScreen(Screen):
     pass
 
+class CreateAccountScreen2(Screen):
+    pass
+
+
 class BookAppointment(Screen):
     pass
 
@@ -22,9 +26,11 @@ class TestApp(App):
         self.sm = ScreenManager()
         self.login_screen = LoginScreen(name='login')
         self.create_account_screen = CreateAccountScreen(name='create_account')
+        self.create_account_screen2 = CreateAccountScreen2(name='create_account2')
         self.book_appointment_screen = BookAppointment(name='book_appointment')  # Corrected typo here
         self.sm.add_widget(self.login_screen)
         self.sm.add_widget(self.create_account_screen)
+        self.sm.add_widget(self.create_account_screen2)
         self.sm.add_widget(self.book_appointment_screen)
         return self.sm
 
